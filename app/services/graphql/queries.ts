c1c804cf2a564a420317ff1697396f7b24cbb8c8
Articunatu <62301779+Articunatu@users.noreply.github.com>
2024-12-01 19:24:06 +0100

    .

--- app/services/graphql/queries.ts
+++ app/services/graphql/queries.ts
@@ -12,3 +12,23 @@ export const GET_TOURNAMENT = gql`
     }
   }
 `;
+
+export const GET_RESULTS_BY_TOURNAMENT_ID = gql`
+  query Evo2018TopPlayers {
+    tournament(slug: "evo-2018") {
+      events(filter: { videogameId: 1 }) { # Melee's game ID
+        id
+        name
+        standings(query: { perPage: 24 }) {
+          nodes {
+            placement
+            entrant {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`
